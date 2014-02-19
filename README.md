112738b863ef7ad5b5f52d8f1b395ce710a4338f
Gabriele Genta <gabriele.genta@gmail.com>
2014-02-19 09:37:38 +0100

    Code cleanup and new readme.

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 > Preprocessor that locates and loads existing source map files.
 
+## Why
+
+When you use karma not in isolation but as part of a build process (e.g. using grunt
+or gulp) it is often the case that the compilation/transpilation is done on a previous
+step of the process and not handled by karma preprocessors. In these cases source maps
+don't get loaded by karma and you lose the advantages of having them.
+
+## How it works
+
+Right now source maps are located via this rule: the preprocessors appends
+".map" to the file name, and if that file exists it loads and includes it.
+
+So if for example you have Hello.js, the preprocessor will try to load source map from
+Hello.js.map.
+
+This is very simple but should work in the majority of cases for standard usage (e.g. with typescript).
+
 ## Installation
 
 Just write `karma-sourcemap-loader` as a devDependency in your `package.json`.
@@ -20,7 +37,7 @@ npm install karma-sourcemap-loader --save-dev
 
 ## Configuration
 
-The code below shows the sample configuration of the preprocessor.
+The code below shows a sample configuration of the preprocessor.
 ```js
 // karma.conf.js
 module.exports = function(config) {
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-//var coffee = require('coffee-script');
 var fs = require('fs');
 var createSourceMapLocatorPreprocessor = function(args, logger, helper) {
   var log = logger.create('preprocessor.sourcemap');
 
   return function(content, file, done) {
-    //console.log('Processing "%s".', file.originalPath);
-    log.debug('Processing "%s".', file.originalPath);
     var mapPath = file.path+".map";
     fs.exists(mapPath, function(exists) {
       if (!exists) {
@@ -19,51 +16,6 @@ var createSourceMapLocatorPreprocessor = function(args, logger, helper) {
       });
     });
   };
-  /*
-  config = config || {};
-
-  var log = logger.create('preprocessor.coffee');
-  var defaultOptions = {
-    bare: true,
-    sourceMap: false
-  };
-  var options = helper.merge(defaultOptions, args.options || {}, config.options || {});
-
-  var transformPath = args.transformPath || config.transformPath || function(filepath) {
-    return filepath.replace(/\.coffee$/, '.js');
-  };
-
-  return function(content, file, done) {
-    var result = null;
-    var map;
-    var datauri;
-
-    log.debug('Processing "%s".', file.originalPath);
-    file.path = transformPath(file.originalPath);
-
-    // Clone the options because coffee.compile mutates them
-    var opts = helper._.clone(options)
-
-    try {
-      result = coffee.compile(content, opts);
-    } catch (e) {
-      log.error('%s\n  at %s:%d', e.message, file.originalPath, e.location.first_line);
-      return;
-    }
-
-    if (result.v3SourceMap) {
-      map = JSON.parse(result.v3SourceMap)
-      map.sources[0] = path.basename(file.originalPath)
-      map.sourcesContent = [content]
-      map.file = path.basename(file.path)
-      file.sourceMap = map;
-      datauri = 'data:application/json;charset=utf-8;base64,' + new Buffer(JSON.stringify(map)).toString('base64')
-      done(result.js + '\n//@ sourceMappingURL=' + datauri + '\n');
-    } else {
-      done(result.js || result)
-    }
-  };
-  */
 };
 
 createSourceMapLocatorPreprocessor.$inject = ['args', 'logger', 'helper'];
