9e3cc12f420b138ef84eae14b859841ffcad4027
Simen Bekkhus <sbekkhus91@gmail.com>
2015-10-10 11:48:46 +0200

    Drop usage of regex.exec and string.match

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 
-var createSourceMapLocatorPreprocessor = function(args, logger, helper) {
+var sourcemapUrlRegeExp = /^\/\/#\s*sourceMappingURL=/;
+
+var createSourceMapLocatorPreprocessor = function(args, logger) {
   var log = logger.create('preprocessor.sourcemap');
 
   return function(content, file, done) {
@@ -40,12 +42,16 @@ var createSourceMapLocatorPreprocessor = function(args, logger, helper) {
 
     var lines = content.split(/\n/);
     var lastLine = lines.pop();
-    while (new RegExp("^\\s*$").test(lastLine)) {
+    while (/^\s*$/.test(lastLine)) {
       lastLine = lines.pop();
     }
 
-    var match = /^\/\/#\s*sourceMappingURL=(.+)$/.exec(lastLine);
-    var mapUrl = match && match[1];
+    var mapUrl;
+
+    if (sourcemapUrlRegeExp.test(lastLine)) {
+      mapUrl = lastLine.replace(sourcemapUrlRegeExp, '');
+    }
+
     if (!mapUrl) {
       fileMap(file.path + ".map");
     } else if (/^data:application\/json/.test(mapUrl)) {
@@ -56,7 +62,7 @@ var createSourceMapLocatorPreprocessor = function(args, logger, helper) {
   };
 };
 
-createSourceMapLocatorPreprocessor.$inject = ['args', 'logger', 'helper'];
+createSourceMapLocatorPreprocessor.$inject = ['args', 'logger'];
 
 // PUBLISH DI MODULE
 module.exports = {
